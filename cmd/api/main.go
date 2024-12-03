d282cb99625dcd227bae0b8f759daa2ed0937ef5
Ryan Sturgess <ryanleo.nz.rs@gmail.com>
2024-12-04 08:28:43 +1300

    Fixed minutes default value

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -16,7 +16,7 @@ func main() {
 				"postgres://mock:mock@localhost/social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
-			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
 	}
 
