a507038e8467c1eaecb4be008787c0301d83d3ab
Ryan Sturgess <ryanleo.nz.rsg@gmail.com>
2024-12-05 10:58:59 +1300

    3 db connection pool todo (#4)
    * Database connection creation code
    
    * DB hooked in
    
    * Postgres docker compose added
    
    * Fixed compose build
    
    * Simple DB social script
    
    * Fixed minutes default value
    
    * Defer database close for closing connection
    
    ---------
    
    Co-authored-by: Pizza-NZ <ryanleo.nz.rs@gmail.com>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/pizza-nz/go-social/internal/db"
 	"github.com/pizza-nz/go-social/internal/env"
 	"github.com/pizza-nz/go-social/internal/store"
 )
@@ -15,11 +16,24 @@ func main() {
 				"postgres://mock:mock@localhost/social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
-			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxIdleConns,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Print("DB connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
